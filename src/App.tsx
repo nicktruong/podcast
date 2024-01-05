8fb0c14565bb0258145eb57b1456c034ea0852f7
Nick <nick.truong.gos@gmail.com>
2024-01-05 08:57:31 +0700

    chore: setup sentry

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { useTranslation } from "react-i18next";
+import { ErrorBoundary } from "@sentry/react";
 
 import logo from "./logo.svg";
 
@@ -9,24 +10,26 @@ function App() {
   const { t } = useTranslation();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Suspense fallback="loading">
-            <h1>{t("learnReact")}</h1>
-          </Suspense>
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary fallback={<p>An error has occurred</p>}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Suspense fallback="loading">
+              <h1>{t("learnReact")}</h1>
+            </Suspense>
+          </a>
+        </header>
+      </div>
+    </ErrorBoundary>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./i18n";
+import "./sentry";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
