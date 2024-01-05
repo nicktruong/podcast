844eef965c3cc3e6d67398707a2d6479620adae5
nicktruong <154394020+nicktruong@users.noreply.github.com>
2024-01-05 09:18:13 +0700

    Chore: Setup eslint + prettier + vscode (#2)
    * chore: setup eslint
    
    * chore: setup prettier
    
    * chore: vscode auto fix eslint and format prettier
    
    * chore: add prettier config file

---
+++ .eslintrc.js
@@ -0,0 +1,88 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:css-import-order/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@stylistic", "@typescript-eslint", "react"],
+  rules: {
+    "arrow-parens": ["error", "always"],
+    curly: "error",
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+      },
+    ],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "block-like",
+        next: "*",
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "block-like",
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "return",
+      },
+    ],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "sort-imports": [
+      "error",
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./",
+      },
+    },
+  },
+};
