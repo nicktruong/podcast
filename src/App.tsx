8a27fe9e0dff98637b596ae115cbad7da68f6f9a
Nick <nick.truong.gos@gmail.com>
2024-01-09 09:31:05 +0700

    chore: setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { ErrorBoundary } from "@sentry/react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import { store } from "@/store/store";
 import router from "@/config/router";
 
 import theme from "./theme";
@@ -12,7 +15,9 @@ function App() {
   return (
     <ErrorBoundary fallback={<p>An error has occurred</p>}>
       <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
         <CssBaseline />
       </ThemeProvider>
     </ErrorBoundary>
