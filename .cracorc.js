fcd7a969436b910f46488ac97246b948b71a1a3b
Nick <nick.truong.gos@gmail.com>
2024-02-02 12:07:07 +0700

    chore: custom envs name

--- .cracorc.js
+++ .cracorc.js
@@ -1,9 +1,28 @@
 const path = require("path");
 
+const dotenv = require("dotenv");
+const webpack = require("webpack");
+
+let result = dotenv.config({ path: path.join(process.cwd(), "./.env") });
+
+if (result.error) {
+  result.parsed = {};
+}
+
+const envKeys = Object.keys(result.parsed).reduce((prev, next) => {
+  // first we search for each key inside of .env.local, because of precedence
+  prev[`process.env.${next.trim()}`] = JSON.stringify(
+    result.parsed[next].trim()
+  );
+
+  return prev;
+}, {});
+
 module.exports = {
   webpack: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.join(process.cwd(), "src"),
     },
+    plugins: [new webpack.DefinePlugin(envKeys)],
   },
 };
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -6,9 +6,7 @@ if ("serviceWorker" in navigator) {
   // Register a service worker hosted at the root of the
   // site using the default scope.
   navigator.serviceWorker.register(swUrl).then(
-    (registration) => {
-      console.log("Service worker registration succeeded:", registration);
-    },
+    (_registration) => {},
     /*catch*/ (error) => {
       console.error(`Service worker registration failed: ${error}`);
     }
